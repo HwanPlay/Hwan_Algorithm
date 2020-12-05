e2382cd71e6a375a73822b2a9fc34fd382403121
이태환 <lwh1992@naver.com>
2020-12-05 18:00:03 +0900

    19238 BFS, Simulation, exception

--- BOJ/19238.py
+++ BOJ/19238.py
@@ -13,87 +13,97 @@ for _ in range(M):
 
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
-def find_passenger(pos_taxi, passenger_arr):
-    sx, sy = pos_taxi
-    if passenger_arr[sx][sy]:
-        dest = passenger_arr[sx][sy]
-        return [sx, sy, 0], dest
+def isPassenger(x, y, passenger_arr):
+    if passenger_arr[x][y]:
+        return True
+    return False
 
-    distance_arr = [[-1]*N for _ in range(N)]
-    Q = deque([[sx, sy]])
-    distance_arr[sx][sy] = 0
-    min_dis_passenger = [-1,-1,float('INF')]  # r,c,distance
+def changeTarget(x,y,d, nx,ny,nd):
+    if nd < d:
+        return nx, ny, nd
+    elif nd == d:
+        if nx < x:
+            return nx, ny, nd
+        elif nx==x:
+            if ny < y:
+                return nx,ny,nd
+    return x,y,d
+
+def find_passenger(pos_taxi, passenger_arr, map_arr):
+    '''
+    taxi 위치에서 승객까지 거리를 찾는다.
+    타겟 승객의 위치, 거리를 반환,
+    '''
+    x, y = pos_taxi
+
+    if passenger_arr[x][y]:  # 택시위치에 승객이 있을때
+        return [x, y], 0
+    distance_arr = [[0] * N for _ in range(N)]
+    check_arr = [[False] * N for _ in range(N)]
+    Q = deque([[x, y]])
+    check_arr[x][y] = True
+    px, py, pdistance = N, N, N**2
     while Q:
-        x, y = Q.popleft()
-        if distance_arr[x][y] > min_dis_passenger[2]:
-            break
+        [x, y] = Q.popleft()
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
-            if not (0<=nx<N and 0<=ny<N): continue
-            if arr[nx][ny] or distance_arr[nx][ny]>=0: continue
-            # arr 1이면 벽, distance 양수이면 이미 방문한 곳
-            if passenger_arr[nx][ny]:
-                if distance_arr[x][y] + 1 < min_dis_passenger[2]:
-                    min_dis_passenger = [nx,ny,distance_arr[x][y] + 1]
-                elif distance_arr[x][y] + 1 == min_dis_passenger[2]:
-                    if nx < min_dis_passenger[0]:
-                        min_dis_passenger[:2] = nx, ny
-                    elif nx == min_dis_passenger[0]:
-                        if ny < min_dis_passenger[1]:
-                            min_dis_passenger[1] = ny
-            Q.append([nx, ny])
+            if not (0<=nx<N and 0<=ny<N) or check_arr[nx][ny] or \
+                    map_arr[nx][ny]:
+                continue
+
+            if isPassenger(nx,ny, passenger_arr):
+                px, py, pdistance = changeTarget(px, py, pdistance, nx, ny, distance_arr[x][y] + 1)
+
+            check_arr[nx][ny] = True
             distance_arr[nx][ny] = distance_arr[x][y] + 1
-    dest = passenger_arr[min_dis_passenger[0]][min_dis_passenger[1]]
-    passenger_arr[min_dis_passenger[0]][min_dis_passenger[1]] = 0
-    if min_dis_passenger[:2] == [-1, -1]:
-        return -1, -1
-    return min_dis_passenger, dest
+            Q.append([nx, ny])
 
-def go_to_dest(pos_taxi, dest_pos,):
-    # print(, pos_taxi, dest_pos,)
-    Q = deque([pos_taxi])
-    dist_arr = [[-1]*N for _ in range(N)]
-    dist_arr[pos_taxi[0]][pos_taxi[1]] = 0
-    # print(Q)
+    return [px, py], pdistance
+
+def go_to_dest(pos_taxi, dest_pos, arr):
+    x, y = pos_taxi
+    Q = deque([[x, y]])
+    distance_arr = [[0]*N for _ in range(N)]
+    check_arr = [[False]*N for _ in range(N)]
+    check_arr[x][y] = True
+
     while Q:
         x, y = Q.popleft()
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
-            if [nx, ny] == dest_pos:
-                return dist_arr[x][y] + 1
-            if not (0<=nx<N and 0<=ny<N): continue
-            if arr[nx][ny] or dist_arr[nx][ny] != -1: continue
-            dist_arr[nx][ny] = dist_arr[x][y] + 1
-            Q.append([nx, ny])
-    if dist_arr[dest_pos[0]][dest_pos[1]] == -1:
-        return -1
-    return dist_arr[dest_pos[0]][dest_pos[1]]
+            if not (0<=nx<N and 0<=ny<N) or check_arr[nx][ny] or \
+                arr[nx][ny]: continue
+            distance_arr[nx][ny] = distance_arr[x][y] + 1
+            check_arr[nx][ny] = True
+            Q.append([nx,ny])
+    tar_x, tar_y = dest_pos
+    if check_arr[tar_x][tar_y]:
+        return [tar_x, tar_y], distance_arr[tar_x][tar_y]
+    else:
+        return [N,N], N**2
 
 
-Fail = False
+Flag = True
 while M:
     M -= 1
-    passenger_pos, dest_pos = find_passenger(pos_taxi, passenger_arr)
+    pos_taxi, dis_passenger = find_passenger(pos_taxi, passenger_arr, arr)
     # x, y, 거리, # 목표 x, y
+    if pos_taxi == [N, N] or dis_passenger == N**2:
+        Flag = False
+        break
+    dest = passenger_arr[pos_taxi[0]][pos_taxi[1]]
+    passenger_arr[pos_taxi[0]][pos_taxi[1]] = 0
 
-    if passenger_pos == -1 or passenger_pos[2] > K:
-        Fail = True
+    pos_taxi, dis_target = go_to_dest(pos_taxi, dest, arr)
+    if pos_taxi == [N, N] or dis_target == N**2:
+        Flag = False
         break
-    K -= passenger_pos[2]
-    pos_taxi = passenger_pos[:2]
-    dest = go_to_dest(pos_taxi, dest_pos)
-    if dest == -1:
-        Fail = True
-        break
-    pos_taxi = dest_pos
-    if K >= dest:
-        K += dest
+    if K - dis_passenger - dis_target >= 0:
+        K += - dis_passenger + dis_target
     else:
-        Fail = True
+        Flag = False
         break
-    # print('dest', dest, Fail)
-
-if Fail:
-    print(-1)
-else:
+if Flag:
     print(K)
+else:
+    print(-1)
